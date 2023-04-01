3a6831b7c41901f529193ede3ea0220a3977d733
nikleodeon <nikleodeon@users.noreply.github.com>
2023-03-31 18:03:29 -0700

    Create game

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let playerWeapon;
+let ominousMessage;
 let randomNumber;
+let computerWeapon;
+let outcome;
+
+function getPlayerChoice() {
+    playerWeapon = prompt("Choose your weapon", "rock, paper, or scissors?");
+
+    if (playerWeapon !== 'rock' || playerWeapon !== 'paper' || playerWeapon !== 'scissors') {
+        ominousMessage = "Choose a proper weapon.";
+    }
+}
 
 function getRandomNumber() {
     randomNumber = Math.random() * 10;
@@ -8,7 +19,7 @@ function getRandomNumber() {
 }
 
 function getComputerChoice(randomNumber) {
-    let computerWeapon = randomNumber;
+    computerWeapon = randomNumber;
 
     if (computerWeapon < 3) {
         computerWeapon = 'rock';
@@ -19,4 +30,40 @@ function getComputerChoice(randomNumber) {
     }
 
     return computerWeapon;
+}
+
+function playGame(playerWeapon, computerWeapon) {
+    if (playerWeapon === 'rock') {
+        if (computerWeapon === 'scissors') {
+            outcome = "Player wins!";
+        } else if (computerWeapon === 'paper'){
+            outcome = "Computer wins!";
+        } else {
+            outcome = "Tragic. Everyone loses."
+        }
+
+        return outcome;
+    } else if (playerWeapon === 'paper') {
+        if (computerWeapon === 'rock') {
+            outcome = "Player wins!";
+        } else if (computerWeapon === 'scissors') {
+            outcome = "Computer wins!";
+        } else {
+            outcome = "Tragic. Everyone loses."
+        }
+
+        return outcome;
+    } else if (playerWeapon === 'scissors') {
+        if (computerWeapon === 'rock') {
+            outcome = "Computer wins!";
+        } else if (computerWeapon === 'paper') {
+            outcome = "Player wins!";
+        } else {
+            outcome = "Tragic. Everyone loses."
+        }
+
+        return outcome;
+    } else if (playerWeapon !== 'rock' || playerWeapon !== 'paper' || playerWeapon !== 'scissors') {
+        return ominousMessage;
+    }
 }
